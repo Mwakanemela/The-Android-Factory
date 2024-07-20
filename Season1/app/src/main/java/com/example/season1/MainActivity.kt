8fb9ba0acf9e38391688dd9e9dcdcadb65bbb534
Mwakanemela <kayangemwakanemela@gmail.com>
2024-07-20 04:32:20 -0700

    S1 E5: Cardview & UI redesign

--- Season1/app/src/main/java/com/example/season1/MainActivity.kt
+++ Season1/app/src/main/java/com/example/season1/MainActivity.kt
@@ -1,19 +1,13 @@
 package com.example.season1
 
 import android.os.Bundle
-import android.widget.Button
-import android.widget.EditText
-import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var button: Button
-    private lateinit var editText: EditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,13 +18,6 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        editText = findViewById(R.id.editText)
-        button = findViewById(R.id.button)
 
-        button.setOnClickListener {
-            val input = editText.text.toString()
-//            Toast.makeText(it.context, input, Toast.LENGTH_SHORT).show()
-            Snackbar.make(it, input, Snackbar.LENGTH_SHORT).show()
-        }
     }
 }
