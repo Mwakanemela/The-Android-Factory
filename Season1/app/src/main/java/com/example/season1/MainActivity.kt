642d337815c55e09446fd33c09117ea4d247026b
Mwakanemela <kayangemwakanemela@gmail.com>
2024-07-20 00:05:01 -0700

    S1 E3: EditText & Toast vs. Snackbar

--- Season1/app/src/main/java/com/example/season1/MainActivity.kt
+++ Season1/app/src/main/java/com/example/season1/MainActivity.kt
@@ -1,12 +1,20 @@
 package com.example.season1
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var button: Button
+    private lateinit var editText: EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -15,6 +23,14 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        editText = findViewById(R.id.editText)
+        button = findViewById(R.id.button)
+
+        button.setOnClickListener {
+            val input = editText.text.toString()
+//            Toast.makeText(it.context, input, Toast.LENGTH_SHORT).show()
+            Snackbar.make(it, input, Snackbar.LENGTH_SHORT).show()
         }
     }
 }
