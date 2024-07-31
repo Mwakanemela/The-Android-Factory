bb20047b781257fcef2375c21456279d437b82f0
Mwakanemela <kayangemwakanemela@gmail.com>
2024-07-31 03:00:32 -0700

    S1 E11: RecylcerView Part 4: changing color of recyclerview itemview

--- Season1/app/src/main/java/com/example/season1/adapter/SimpleAdapter.kt
+++ Season1/app/src/main/java/com/example/season1/adapter/SimpleAdapter.kt
@@ -6,7 +6,7 @@ import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.season1.R
 
-class SimpleAdapter: RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+class SimpleAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 
     private val data = ArrayList<String>()
 
@@ -19,21 +19,28 @@ class SimpleAdapter: RecyclerView.Adapter<RecyclerView.ViewHolder>() {
     }
 
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-        (holder as SimpleViewHolder).onBind(data[position])
+        (holder as SimpleViewHolder).onBind(data[position], position)
     }
 
     fun setData(data: List<String>) {
         this.data.clear()
         this.data.addAll(data)
     }
-    inner class SimpleViewHolder(parent: ViewGroup): RecyclerView.ViewHolder(
+
+    inner class SimpleViewHolder(parent: ViewGroup) : RecyclerView.ViewHolder(
         LayoutInflater.from(parent.context).inflate(R.layout.view_holder_textview, parent, false)
     ) {
 
         private val textView: TextView = itemView.findViewById(R.id.textView)
-        fun onBind(textToDisplay: String) {
+        fun onBind(textToDisplay: String, position: Int) {
 
+            val color = if (position % 2 == 0) {
+                itemView.context.getColor(R.color.light_blue)
+            } else {
+                itemView.context.getColor(R.color.white)
+            }
             textView.text = textToDisplay
+            textView.setBackgroundColor(color)
         }
     }
 }
