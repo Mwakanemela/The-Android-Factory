431c29bb8b5442dba72c29e4cf913ab94a888c75
Mwakanemela <kayangemwakanemela@gmail.com>
2024-08-01 06:48:36 -0700

    S2 E0: Project Creation -> set up a few things and data class

---
+++ SeasonTwo/app/src/main/java/com/example/seasontwo/dataclass/Tile.kt
@@ -0,0 +1,10 @@
+package com.example.seasontwo.dataclass
+
+data class Tile (
+    val id: String = "",
+    val headerImageResourceId: Int = 0,
+    val title:String = "",
+    val description: String = "",
+    val descriptionLong: String = "",
+    val buttonText:String = ""
+)
